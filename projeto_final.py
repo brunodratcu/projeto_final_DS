15fb15f17a1e8193878fd279e7f882b46d0c5acb
brunodratcu <btdratcu@hotmail.com>
2016-04-27 11:31:25 -0300

    comeco do projeto

--- projeto_final.py
+++ projeto_final.py
@@ -5,8 +5,27 @@ Created on Wed Apr 20 11:34:01 2016
 @author: Bruno Dratcu
 """
 
-a = "teste"
-b = "funcionando?"
+import webbrowser
 
-print(a)
-print(b)
+class Pagina():
+    """Classe que irá abrir uma pagina para mostrar dados de jogos"""
+    
+    def __init__(self, precos, jogos, descricao, cadastro):
+        self.precos = precos
+        self.jogos = jogos
+        self.descricao = descricao
+        self.cadastro = cadastro
+        
+    def mostrar_precos(self):
+        
+    
+    def titulo_jogos(self):
+        
+    
+    def mostrar_descricao(self):
+        
+    
+    def fazer_cadastro(self):
+        
+    
+    
