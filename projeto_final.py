c9408c959a682e5fea713e4cf43e50fbb450ab6e
brunodratcu <btdratcu@hotmail.com>
2016-04-30 12:53:31 -0300

    arrumando

--- projeto_final.py
+++ projeto_final.py
@@ -16,11 +16,42 @@ class Pagina():
         self.poster_url = poster
         self.preco = preco
         self.trailer_url = trailer
+        
+    def show_title(self):
+        if self.title:
+            print(self.poster_url)
+            
+    def show_descricao(self):
+        if self
 
         
     def show_trailer(self):
-        webbrowser.open(self.trailer_url)
+        if self.trailer_url:
+        print(webbrowser.open(self.trailer_url))
         
         
+        
+gta_poster = webbrowser.open("http://cdn.atl.clicrbs.com.br/wp-content/uploads/sites/27/2015/04/actual_1410520494.jpg")
+gta_trailer = webbrowser.open("https://www.youtube.com/watch?v=VjZ5tgjPVfU")
 
-        
+mario_poster = webbrowser.open("https://i.ytimg.com/vi/1dhrHlol3SM/maxresdefault.jpg")
+mario_trailer = webbrowser.open("https://www.youtube.com/watch?v=eO8xe2AUY4c")
+
+
+GTA = Pagina("GTA", 
+             "Jogo que mata pessoas", 
+             "gta_poster", 
+#             "http://cdn.atl.clicrbs.com.br/wp-content/uploads/sites/27/2015/04/actual_1410520494.jpg"
+             "R$200,00", 
+             "gta_trailer")
+#             "https://www.youtube.com/watch?v=VjZ5tgjPVfU")
+                           
+#print(GTA.title)
+
+mario_bros = Pagina("Super Mario Bros", 
+                    "Jogo de um encanador psicodelico", 
+                    "mario_poster", 
+#                    "https://i.ytimg.com/vi/1dhrHlol3SM/maxresdefault.jpg"
+                    "R$150,00", 
+                    "mario_trailer")
+#                    "https://www.youtube.com/watch?v=eO8xe2AUY4c")
