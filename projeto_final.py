ef3d0676bc88edbc7d3ff1bca83e525bd9632485
brunodratcu <btdratcu@hotmail.com>
2016-05-04 10:54:26 -0300

    adicionando pac-man

--- projeto_final.py
+++ projeto_final.py
@@ -55,26 +55,43 @@ mario_bros = Pagina(True,
                     "Super Mario Bros",
                     "Jogo de um encanador psicodelico", 
                     "R$150,00",
-                    "https://i.ytimg.com/vi/1dhrHlol3SM/maxresdefault.jpg",
-                    "https://www.youtube.com/watch?v=eO8xe2AUY4c")
-
+                    "https://www.youtube.com/watch?v=eO8xe2AUY4c",
+                    "https://i.ytimg.com/vi/1dhrHlol3SM/maxresdefault.jpg")
+                    
+pac_man = Pagina(True,
+                 "Pac-Man",
+                 "Bolinha amarela que fica numa sala escura e quando come um doce tem poderes para comer fantasmas",
+                 "R$100,00",
+                 "https://www.youtube.com/watch?v=CFWnTu_42d0",
+                 "http://www.redefonte.com/wp-content/uploads/2013/06/Pacman-Online.jpg")
+                    
 print(gta.titulo)
-print(gta.descricao)
-print(gta.preco)
-print(gta.trailer)
-print(gta.poster)
-gta.show_trailer()
-gta.show_trailer
+#print(gta.descricao)
+#print(gta.preco)
+#print(gta.trailer)
+#print(gta.poster)
+#gta.show_trailer()
 gta.show_poster()
 
 print()
 
 print(mario_bros.titulo)
-print(mario_bros.descricao)
-print(mario_bros.preco)
-print(mario_bros.trailer)
-print(mario_bros.poster)
-mario_bros.show_trailer()
-mario_bros.show_trailer
-mario_bros.show_poster()    
+#print(mario_bros.descricao)
+#print(mario_bros.preco)
+#print(mario_bros.trailer)
+#print(mario_bros.poster)
+#mario_bros.show_trailer()
+mario_bros.show_poster()
+
+print()
+
+print(pac_man.titulo)
+#print(pac_man.descricao)
+#print(pac_man.preco)
+#print(pac_man.trailer)
+#print(pac_man.poster)
+#pac_man.show_trailer()
+pac_man.show_poster()
+
+    
 
