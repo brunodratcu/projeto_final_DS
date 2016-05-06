f760cf6f72f64161996d97f05808e35d230ceba5
brunodratcu <btdratcu@hotmail.com>
2016-05-06 11:37:51 -0300

    adicionando site nos precos

--- projeto_final.py
+++ projeto_final.py
@@ -37,7 +37,7 @@ class Pagina:
     
     def show_preco(self):
         if self.titulo:
-            print(self.preco)
+            webbrowser.open(self.preco)
             
     def show_trailer(self):
         if self.titulo:
@@ -50,11 +50,13 @@ class Pagina:
     def show_buscar_jogos(self):
         self.buscar_jogos = True
         if nomes_jogos == "GTA" or nomes_jogos == "gta":
-                gta.show_descricao()
+            gta.show_descricao()
         elif nomes_jogos == "Mario Bros" or nomes_jogos == "mario bros":
-                mario_bros.show_descricao()
+            mario_bros.show_descricao()
         elif nomes_jogos == "pac man" or nomes_jogos == "Pac Man" or nomes_jogos == "Pac-Man":
-                pac_man.show_descricao()
+            pac_man.show_descricao()
+        elif nomes_jogos == "call of duty" or nomes_jogos == "Call of Duty":
+            call_of_duty.show_descricao()
         else:
             print("Jogo não encontrado!")
             
@@ -63,9 +65,9 @@ nomes_jogos = input("Qual o game desejado? ")
 
 
 gta = Pagina(True, 
-             "GTA", 
+             "GTA V", 
              "Jogo de um maniaco que mata todo mundo", 
-             "R$200,00", 
+             "http://www.uzgames.com.br/gta?&utmi_p=_games&utmi_pc=BuscaFullText&utmi_cp=gta", 
              "https://www.youtube.com/watch?v=VjZ5tgjPVfU", 
              "http://cdn.atl.clicrbs.com.br/wp-content/uploads/sites/27/2015/04/actual_1410520494.jpg",
              True)
@@ -73,7 +75,7 @@ gta = Pagina(True,
 mario_bros = Pagina(True,
                     "Super Mario Bros",
                     "Jogo de um encanador psicodelico", 
-                    "R$150,00",
+                    "http://www.uzgames.com.br/mario%20bros?&utmi_p=_gta&utmi_pc=BuscaFullText&utmi_cp=mario%20bros",
                     "https://www.youtube.com/watch?v=eO8xe2AUY4c",
                     "https://i.ytimg.com/vi/1dhrHlol3SM/maxresdefault.jpg",
                     True)
@@ -81,15 +83,15 @@ mario_bros = Pagina(True,
 pac_man = Pagina(True,
                  "Pac-Man",
                  "Bolinha amarela que fica numa sala escura e quando come um doce tem poderes para comer fantasmas",
-                 "R$100,00",
+                 "http://www.uzgames.com.br/pac%20man?&utmi_p=_gta&utmi_pc=BuscaFullText&utmi_cp=pac%20man",
                  "https://www.youtube.com/watch?v=CFWnTu_42d0",
                  "http://www.redefonte.com/wp-content/uploads/2013/06/Pacman-Online.jpg",
                  True)
                  
 call_of_duty = Pagina(True,
                       "Call of Duty",
-                      "Jogo na qual vc pode atirar em outros usuarios online",
-                      "R$300,00",
+                      "Jogo no qual vc pode atirar em outros usuarios online",
+                      "http://www.uzgames.com.br/call%20of%20duty%20ghosts?&utmi_p=_call+of+duty+gosth&utmi_pc=BuscaFullText&utmi_cp=call%20of%20duty%20ghosts",
                       "https://www.youtube.com/watch?v=ktz5G24BOsg",
                       "http://cdn2-www.comingsoon.net/assets/uploads/2015/04/cod-bo3-header2.jpg",
                       True)
@@ -100,52 +102,56 @@ mario_bros.show_buscar_jogos()
 pac_man.show_buscar_jogos()
 call_of_duty.show_buscar_jogos()
 
-print()
-                      
-
-print(gta.titulo)
-print(gta.descricao)
-print(gta.preco)
+#print()
+#                      
+#
+#print(gta.titulo)
+#print(gta.descricao)
+#print(gta.preco)
 #print(gta.trailer)
 #print(gta.poster)
-                    
+#                    
 #gta.show_trailer()
 #gta.show_poster()
-
-print()
-
-print(mario_bros.titulo)
-print(mario_bros.descricao)
-print(mario_bros.preco)
+#gta.show_preco()
+#
+#print()
+#
+#print(mario_bros.titulo)
+#print(mario_bros.descricao)
+#print(mario_bros.preco)
 #print(mario_bros.trailer)
 #print(mario_bros.poster)
-
+#
 #mario_bros.show_trailer()
 #mario_bros.show_poster()
-
-print()
-
-print(pac_man.titulo)
-print(pac_man.descricao)
-print(pac_man.preco)
+#mario_bros.show_preco()
+#
+#print()
+#
+#print(pac_man.titulo)
+#print(pac_man.descricao)
+#print(pac_man.preco)
 #print(pac_man.trailer)
 #print(pac_man.poster)
-
+#
 #pac_man.show_trailer()
 #pac_man.show_poster()
-
-print()
-
-print(call_of_duty.titulo)
-print(call_of_duty.descricao)
-print(call_of_duty.preco)
+#pac_man.show_preco()
+#
+#print()
+#
+#print(call_of_duty.titulo)
+#print(call_of_duty.descricao)
+#print(call_of_duty.preco)
 #print(call_of_duty.trailer)
 #print(call_of_duty.poster)
-
+#
 #call_of_duty.show_trailer()
 #call_of_duty.show_poster()
+#call_of_duty.show_preco()
 
     
     
 jogos = [gta, mario_bros, pac_man, call_of_duty]
-projeto_final_2.open_movies_page(jogos)
+#projeto_final_2.open_movies_page(jogos)
