72bd0958be16e56fdbb74742efa528b086cadf05
fspoficial <felipe@speedio.com.br>
2016-05-20 14:39:40 -0300

    Inclusão de plataforma

--- comparapreco.py
+++ comparapreco.py
@@ -1,10 +1,10 @@
-ref_arquivo = open("a.txt","r")
+ref_arquivo = open("banco.txt","r")
 lista_de_linhas = ref_arquivo.readlines()
 valores=[]
 indices=[]
 i=0
 while i<len(lista_de_linhas):
-    if lista_de_linhas[i].split(",")[0]=="PACMAN":
+    if lista_de_linhas[i].split(",")[0]=="FIFA":
        n=float(lista_de_linhas[i].split(",")[1]) 
        valores.append(n)
        indices.append(i)
@@ -13,8 +13,9 @@ while i<len(lista_de_linhas):
 a=valores.index(min(valores))
 b=indices[a]
 preco=lista_de_linhas[b].split(",")[1]
-loja=lista_de_linhas[b].split(",")[2]
-link=lista_de_linhas[b].split(",")[3]
+site=lista_de_linhas[b].split(",")[2]
+loja=lista_de_linhas[b].split(",")[3]
+plataforma=lista_de_linhas[b].split(",")[4]
 print(preco)
 
 ref_arquivo.close()
